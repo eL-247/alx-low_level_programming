13fd509b74e26e35248690b69263e900b4343199
Israel H <israelhailu67@gmail.com>
2022-09-09 01:43:04 -0700

    Added the mandatory task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints some text
+ * Return: 0 if its a sucess
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
