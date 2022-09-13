f18d58902c24e34ff46ba1fc6003aebc811aa792
Israel H <israelhailu67@gmail.com>
2022-09-13 15:29:32 -0700

    Task one

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - main block
@@ -8,6 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
