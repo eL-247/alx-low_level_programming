da2d98ba5bc0d39e46a6d4cff899c04d8ca24c41
Israel H <israelhailu67.git@gmail.com>
2022-10-29 12:44:41 -0700

    9 times tableupdated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,79 +5,32 @@
  * Retun: null
  */
 void times_table(void)
-
 {
+	int x, y, z;
 
-		int x = 0; /* factor */
-
-			int y; /* count  */
-
-				int z; /* computed value */
-
-
-
-					while (x < 10)
-
-							{
-
-										y = 0;
-
-												while (y < 10)
-
-															{
-
-																			z = x * y;
-
-
-
-																						if (z > 9)
-
-																										{
-
-																															_putchar(z / 10 + '0');
-
-																																			_putchar(z % 10 + '0');
-
-																																						}
-
-																									else if (y != 0)
-
-																													{
-
-																																		_putchar(' ');
-
-																																						_putchar(z + '0');
-
-																																									}
-
-																												else
-
-																																{
-
-																																					_putchar(z + '0');
-
-																																								}
-
-
-
-																															if (y != 9)
-
-																																			{
-
-																																								_putchar(',');
-
-																																												_putchar(' ');
-
-																																															}
-
-																																		y++;
-
-																																				}
-
-														_putchar('\n');
-
-																x++;
-
-																	}
-
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				_putchar(',');
+				_putchar(32);
+				_putchar(((z / 10) % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
