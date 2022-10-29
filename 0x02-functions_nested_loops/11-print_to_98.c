09442cf7aeb6ea9c328e09426711662063d26a9b
Israel H <israelhailu67.git@gmail.com>
2022-10-29 11:06:41 -0700

    fixed a betty error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,13 @@
  */
 void print_to_98(int n)
 {
-	while (n != 98) {
+	while (n != 98)
+	{
 		printf("%d, ", n);
 		if (n > 98)
-		{
-			n --;
-		} else {
+			n--;
+		else
 			n++;
-		}
 	}
 	printf("%d\n", 98);
 }
