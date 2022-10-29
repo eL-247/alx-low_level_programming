7a0da27f38b71bba345800e847a94d8a7dc10bfa
Israel H <israelhailu67.git@gmail.com>
2022-10-29 10:36:26 -0700

    added time function

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,69 +1,41 @@
 #include "main.h"
-
 /**
  * jack_bauer - prints all possible times
  * Retun: null
  */
-
 void jack_bauer(void)
 {
-	int x = 0;
+	int a, i, j, k, l;
 
-	int a = 0, b = 0, c = 0, d = 0;
-
-
-
-	while (x < 1440)
-
-				{
-
-							_putchar(a + '0');
-
-									_putchar(b + '0');
-
-											_putchar(':');
-
-													_putchar(c + '0');
-
-															_putchar(d + '0');
-
-																	_putchar('\n');
-
-
-
-																			d++;
-
-																					if (d > 9)
-
-																								{
-
-																												d = 0;
-
-																															c++;
-
-																																	}
-
-																							if (c > 5)
-
-																										{
-
-																														c = 0;
-
-																																	b++;
-
-																																			}
-
-																									if (b > 9)
-
-																												{
-
-																																b = 0;
-
-																																			a++;
-
-																																					}
-
-																											x++;
-
-																												}
+	a = i = j = k = l = 0;
+	while (a < 1440)
+	{
+		if (l > 9)
+		{
+			l = 0;
+			k++;
+		}
+		if (k > 5)
+		{
+			k = 0;
+			j++;
+		}
+		if (j > 9)
+		{
+			j = 0;
+			i++;
+		}
+		if (i == 3)
+		{
+			break;/*not nedded but put it here just in case*/
+		}
+		_putchar(i + '0');
+		_putchar(j + '0');
+		_putchar(':');
+		_putchar(k + '0');
+		_putchar(l + '0');
+		_putchar('\n');
+		l++;
+		a++;/*terminates session*/
+	}
 }
