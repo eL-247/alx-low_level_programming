885ec577578f2c24b9c5933ff97819e0055f802a
Israel H <israelhailu67.git@gmail.com>
2022-10-29 13:20:23 -0700

    fixed a betty error again

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -4,10 +4,9 @@
  * _strpbrk - Search a string for any of a set of bytes.
  * @s: string
  * @accept: string to match
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept` 
+ * Return: Pointer to the byte in `s` that matches one of the bytes in accept
  * or NULL if no such byte is found.
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
